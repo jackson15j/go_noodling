726c2259e59773d7679cb5e9451782cfe394d900
Craig Astill <jackson15j@hotmail.com>
2023-05-19 17:14:23 +0100

    Formatter: Print truncation of all files in a directory.

--- pimoroni_eink_screen_formatter/pimoroni_eink_screen_formatter.go
+++ pimoroni_eink_screen_formatter/pimoroni_eink_screen_formatter.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
 	fmt.Println(Truncate("Hello, World!"))
+	TruncateFilesInFolder("test_data")
 }
 
 // TODO: read/write files.
@@ -17,7 +20,8 @@ func main() {
 func Truncate(line string) (string, error) {
 	if len(line) == 0 {
 		// Avoid runtime slice error if line is empty. Just return.
-		return line, nil}
+		return line, nil
+	}
 	return line[:5], nil
 }
 
@@ -48,3 +52,30 @@ func TruncateLines(text string) (string, error) {
 
 func splitLines(text string) ([]string, error) { return strings.Split(text, "\n"), nil }
 func joinLines(lines []string) (string, error) { return strings.Join(lines[:], "\n"), nil }
+
+func TruncateFilesInFolder(dir string) error {
+	files, err := os.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+
+	for _, file := range files {
+		path := filepath.Join(dir, file.Name())
+		// TODO: collect errors into a slice report at the end.
+		fmt.Printf("Truncating: %s...\n", path)
+		content, err := os.ReadFile(path)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		truncated_text, err := TruncateLines(string(content))
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		fmt.Printf("Truncated content for: %s: %s\n", path, truncated_text)
+		// TODO: Write truncated text to a file.
+	}
+	return nil
+}
