2a3a39677c5e605fa37f50bfcadd8f50f3045af0
Craig Astill <jackson15j@hotmail.com>
2023-05-19 16:31:08 +0100

    Formatter: test `TruncateLines()` with a real file.

--- pimoroni_eink_screen_formatter/pimoroni_eink_screen_formatter_test.go
+++ pimoroni_eink_screen_formatter/pimoroni_eink_screen_formatter_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"testing"
 )
 
@@ -26,6 +27,18 @@ func TestTruncateLinesHandleEmptyLines(t *testing.T) {
 	text := "First long line.\n\n\n"
 	exp := "First\n\n\n"
 	msg, err := TruncateLines(text)
+	if msg != exp || err != nil {
+		t.Fatalf(`TruncateLines(text) = %q, %v, want exp, error`, msg, err)
+	}
+}
+
+func TestTruncateLinesFromAFile(t *testing.T) {
+	content, err := os.ReadFile("test_data/file1.txt")
+	if err != nil {
+		t.Fatalf(`Reading test file failed: %v`, err)
+	}
+	exp := "1st l\n2nd l\n"
+	msg, err := TruncateLines(string(content))
 	if msg != exp || err != nil {
 		t.Fatalf(`TruncateLines(text) = %q, %v, want exp, error`, msg, err)
 	}
