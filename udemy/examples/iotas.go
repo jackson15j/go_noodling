8c4b8333c4286be0ec05b02c371d50a26462f0e7
Craig Astill <jackson15j@hotmail.com>
2023-07-10 10:10:12 +0100

    Use pointer receiver for String output + units on `ByteSize` type.

--- udemy/examples/iotas.go
+++ udemy/examples/iotas.go
@@ -1,3 +1,4 @@
+// https://go.dev/doc/effective_go#constants
 package examples
 
 import "fmt"
@@ -15,6 +16,35 @@ const (
 	ZB
 	YB
 )
+
+/*
+	Stringify ByteSize value + add unit.
+
+Adding this method onto the `ByteSize` type will change the output of the call
+in `Iota()` from the raw float output, to a nicely string formatted output with
+the unit attached.
+*/
+func (b ByteSize) String() string {
+	switch {
+	case b >= YB:
+		return fmt.Sprintf("%.2fYB", b/YB)
+	case b >= ZB:
+		return fmt.Sprintf("%.2fZB", b/ZB)
+	case b >= EB:
+		return fmt.Sprintf("%.2fEB", b/EB)
+	case b >= PB:
+		return fmt.Sprintf("%.2fPB", b/PB)
+	case b >= TB:
+		return fmt.Sprintf("%.2fTB", b/TB)
+	case b >= GB:
+		return fmt.Sprintf("%.2fGB", b/GB)
+	case b >= MB:
+		return fmt.Sprintf("%.2fMB", b/MB)
+	case b >= KB:
+		return fmt.Sprintf("%.2fKB", b/KB)
+	}
+	return fmt.Sprintf("%.2fB", b)
+}
 
 func Iota() {
 	bss := []ByteSize{KB, MB, GB, TB, PB, EB, ZB, YB}
