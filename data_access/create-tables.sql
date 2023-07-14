1160bbca5ef303e8112f2e6c6422f10b24667649
Craig Astill <jackson15j@hotmail.com>
2023-07-14 11:40:57 +0100

    data-access: Write table into a schema.
    MySQL doesn't have schemas, so needed to update the SQL query a bit more.

--- data_access/create-tables.sql
+++ data_access/create-tables.sql
@@ -1,13 +1,17 @@
 /* https://go.dev/doc/tutorial/database-access */
-DROP TABLE IF EXISTS album;
-CREATE TABLE album (
+DROP TABLE IF EXISTS data_access.album;
+DROP SCHEMA IF EXISTS data_access;
+/* https://www.postgresql.org/docs/current/sql-createschema.html */
+CREATE SCHEMA data_access;
+/* https://www.postgresql.org/docs/current/sql-createtable.html */
+CREATE TABLE data_access.album (
   id         SERIAL primary key,
   title      VARCHAR(128) NOT NULL,
   artist     VARCHAR(255) NOT NULL,
   price      DECIMAL(5,2) NOT NULL
 );
 
-INSERT INTO album
+INSERT INTO data_access.album
   (title, artist, price)
 VALUES
   ('Blue Train', 'John Coltrane', 56.99),
@@ -16,4 +20,4 @@ VALUES
   ('Sarah Vaughan', 'Sarah Vaughan', 34.98);
 
 /* Verification */
-SELECT * FROM album;
+SELECT * FROM data_access.album;
