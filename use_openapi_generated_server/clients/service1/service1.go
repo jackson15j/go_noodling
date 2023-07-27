a452a205c031f4daf5054703571b119d57bbe132
Craig Astill <jackson15j@hotmail.com>
2023-07-27 17:29:30 +0100

    OpenAPI: [BROKEN] Initial attempt to use Client generated code.

--- use_openapi_generated_server/clients/service1/service1.go
+++ use_openapi_generated_server/clients/service1/service1.go
@@ -1,3 +1,17 @@
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -config cfg.yaml openapi3_1.yaml
+
 package service1
 
-//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen -config cfg.yaml openapi3_1.yaml
+import (
+	// "fmt"
+	"github.com/labstack/echo/v4"
+	// "go_noodling/use_openapi_generated_server/clients/service1"
+)
+
+func (p *HealthReadResponse) HealthRead(ctx echo.Context) error {
+	// TODO: Get client implementation into this file!!
+
+	// c, err := service1.NewClient("http://localhost:8085")
+	// fmt.Println(err)
+	return ctx.JSON(p.StatusCode(), p.Body)
+}
