60a518613f75909a9bee6c52d5d02a88f754b1fa
Craig Astill <jackson15j@hotmail.com>
2025-08-08 17:18:56 +0100

    build: Dape profile to attach to the debug container.
    Source mapping from subfolder of go_noodling repo, into the source
    folder the binary was built from in the container.

--- use_openapi_generated_server/.dir-locals.el
+++ use_openapi_generated_server/.dir-locals.el
@@ -19,6 +19,19 @@
                           ;; TODO: Add Tramp config to attach to remote go app.
                           ;; Also need an SSH tunnel! See:
                           ;; https://github.com/svaante/dape/issues/109
+
+                          ;; Profile 2: Attach to external debugger
+                          (go-debug-docker
+                           modes (go-mode go-ts-mode)
+                           command "dlv"
+                           port 40000
+                           :request "attach"  ;; this will run "dlv attach ..."
+                           :mode "remote"     ;; connect to a running debugger session
+                           :type "go"
+                           ;; https://github.com/emacs-lsp/dap-mode/blob/b97756665709bea37b9ffe262c5fa9196f1b4577/docs/page/configuration.md?plain=1#L622
+                           ;; Set `from` as absolute path to repo root and map to code root in container.
+                           :substitutePath (vector (ht ("from" (concat (funcall dape-cwd-fn) "use_openapi_generated_server")) ("to" "/src")))
+                           )
                           )
                        )
          )
